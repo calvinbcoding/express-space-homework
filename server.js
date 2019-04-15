fb2437dfac210b853bb2c143a4d5dfe2f760bfae
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-14 21:54:44 -0600

    "progress"

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const marsMissions = require('./models/marsMissions');
+const bodyParser = require('body-parser');
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -11,15 +12,14 @@ const marsMissions = require('./models/marsMissions');
 // * Make it so you can click on a mission’s name on the index page, and be taken to that mission’s show page
 // * Bonus: add images to the data and have them display
 // * Bonus: add static css to style the pages
-app.get('/marsMissions', (req,res)=>{
-  res.send(marsMissions)
-});
 
-app.get('/marsMissions', (req,res)=>{
-  res.render(`index.ejs`, {
-      "marsMissions": 1
-  });
-});
+// app.post('/marsMissions', function(req, res) {
+//   console.log('CREATE route accessed');
+//   console.log('Data within req.body: ', req.body);
+//   products.push(req.body);
+//   res.redirect('/missions');
+// });
+
 // NOTES:
 // ejs has not been installed
 // views folder has not been created
@@ -28,6 +28,14 @@ app.get('/marsMissions', (req,res)=>{
 // PORT
 const port = 3000;
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// app.get('./marsMissions', (req,res)=>
+//   res.send(marsMissions)
+// });
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
 
@@ -37,15 +45,27 @@ const port = 3000;
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
 
+app.get('/missions', (req,res) => {
+  res.render(`index.ejs`, {
+      missions : marsMissions
+  });
+});
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
-
-
+app.get('/marsMissions/show/:id', (req,res)=>{
+  res.render(`show.ejs`, {
+      marsMissions : marsMissions[req.params.id]
+  });
+});
 
 // LISTENER
 app.listen(port, function() {
   console.log('Missions to Mars running on port: ', port);
-})
+});
+
+
+
+  
 
 
