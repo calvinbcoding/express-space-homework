c09f37987c71701421230e425c62a3dd675df882
Calvin Bruch <calvinbcoding@gmail.com>
2019-04-16 18:28:07 -0600

    test passed

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Run <code>npm install</code> to install necessary packages.
 Run <code>npm test</code> to run automated testing and confirm your assignment meets specs.
 Follow the directions in server.js
+
+
+<h1>The mission <%=marsMissions.name%> hand a launch date of <%=marsMissions.launchDate%>, it was operated by <%=marsMissions.operator%>, and was a <%=marsMissions.type%> mission. <%=marsMissions.img%></h1>
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const marsMissions = require('./models/marsMissions.js');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -13,12 +13,7 @@ const bodyParser = require('body-parser');
 // * Bonus: add images to the data and have them display
 // * Bonus: add static css to style the pages
 
-// app.post('/marsMissions', function(req, res) {
-//   console.log('CREATE route accessed');
-//   console.log('Data within req.body: ', req.body);
-//   products.push(req.body);
-//   res.redirect('/missions');
-// });
+
 
 // NOTES:
 // ejs has not been installed
@@ -29,8 +24,8 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 
 // app.get('./marsMissions', (req,res)=>
@@ -38,7 +33,13 @@ app.use(bodyParser.json());
 // });
 // DATA - put into marsMissions.js file inside of a models folder, for module.exports
 // remember to require it in the server
-
+// app.post('/marsMissions', function(req, res) {
+//     res.send('/marsMissions')
+//   console.log('CREATE route accessed');
+//   console.log('Data within req.body: ', req.body);
+//   marsMissions.push(req.body);
+//   res.redirect('/missions');
+// });
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
@@ -47,8 +48,17 @@ app.use(bodyParser.json());
 // app.get('/marsMissions', (req,res)=>{
 //   res.send(marsMissions)
 // });
+//show route
+app.get('/missions/:id', (req,res)=>{
+  console.log(marsMissions[req.params.id]);
+  res.render('missions/show.ejs', {
+    marsMissions : marsMissions[req.params.id]
+  })
+})
 
-app.get('/marsMissions/', (req,res) => {
+
+//index route
+app.get('/missions/', (req,res) => {
   res.render('missions/index.ejs', {
       marsMissions : marsMissions
   })
@@ -61,11 +71,11 @@ app.get('/marsMissions/', (req,res) => {
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
-app.get('/marsMissions/:id', (req,res)=>{
-  res.render('/missions/show.ejs', {
-      marsMissions : marsMissions[req.params.id]
-  })
-})
+// app.get('/marsMissions/:id', (req,res)=>{
+//   res.render('/missions/show.ejs', {
+//       marsMissions : marsMissions[req.params.id]
+//   })
+// })
 
 // LISTENER
 app.listen(port, function() {
@@ -74,6 +84,6 @@ app.listen(port, function() {
 
 
 
-  
+  module.exports = app;
 
 
